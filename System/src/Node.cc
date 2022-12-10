d5c520a80ef7c3055dbfad993498d3b2e399fff4
Abdullah Adel <abdullahadel.aam@gmail.com>
2022-12-10 03:11:18 +0200

    at 1:35

--- System/src/Node.cc
+++ System/src/Node.cc
@@ -9,137 +9,155 @@ void Node::initialize()
     std::remove("../output/output.txt");
 }
 
+void Node::sendPacket(Packet_Base *packet)
+{
+    // Send the next packet to the other node
+    send(packet, NODE_OUTPUT);
+
+    int TO = getParentModule()->par("TO").intValue();
+    Packet_Base *timeout = new Packet_Base(std::to_string(index).c_str());
+    scheduleAt(simTime() + exponential(TO), timeout);
+
+    // Write the sent message to the output file
+    outputFile << "Sent: " << packet->getPayload() << endl;
+}
+
+void Node::checkTimeout(int msgIndex)
+{
+    EV << "Checking timeout\n";
+    if (msgIndex < this->start)
+        return;
+    else
+    {
+        EV << "Timed out\n";
+        this->start = msgIndex;
+        this->index = msgIndex;
+    }
+}
+
+Packet_Base *Node::createPacket(Packet_Base *oldPacket, std::string newPayload)
+{
+    // 1.Apply byte stuffing framing to the message payload
+    std::string frame = Node::framing(newPayload);
+    // 2.Set the trailer to the parity byte
+    oldPacket->setTrailer(Node::getParity(frame));
+    // 3.Set the payload to the frame
+    oldPacket->setPayload(frame.c_str());
+    // 4.Set the header to the sequence number
+    oldPacket->setHeader(index);
+    // 5.Set the frame type to data
+    oldPacket->setFrame_type(DATA_SIGNAL);
+
+    return oldPacket;
+}
+
+void Node::readFileMsg()
+{
+    std::string inputMessage;
+
+    std::ifstream inputFile;
+
+    // If we are in Node 0, then read the messages from input0.txt
+    if (strcmp(getName(), NODE0) == 0)
+    {
+        inputFile.open("../inputs/input0.txt");
+    }
+    // Else if we are in Node 1, then read messages from input1.txt
+    else if (strcmp(getName(), NODE1) == 0)
+    {
+        inputFile.open("../inputs/input1.txt");
+    }
+
+    // Read messages from the input file
+    while (std::getline(inputFile, inputMessage))
+    {
+        // The first part will be errors
+        std::string errors = strtok((char *)inputMessage.c_str(), " ");
+
+        // The rest of the data will be the message to be sent
+        std::string message = inputMessage.substr(errors.size() + 1);
+
+        // Store the errors-message pair in the data vector
+        data.emplace_back(std::make_pair(errors, message));
+    }
+
+    // Close the input file
+    inputFile.close();
+}
+
+int Node::receivePacket(Packet_Base *packet)
+{
+    int seqNum = packet->getHeader();
+    std::string frame = packet->getPayload();
+    char parity = packet->getTrailer();
+
+    // Check if the parity byte is error free
+    if (Node::checkParity(frame, parity))
+    {
+        outputFile << "Received: " << frame << " with sequence number " << seqNum << endl;
+        return seqNum;
+    }
+
+    outputFile << "Error detected!" << endl;
+    return -1;
+}
+
+void Node::sendAck(Packet_Base *packet, int seqNum)
+{
+    // If the received message is corrupted, then don't send an ACK
+    if (seqNum != expectedSeqNum)
+        return;
+
+    // Increment the expected sequence number
+    expectedSeqNum++;
+
+    // Send an ACK signal to the sender node to send the next message
+    packet->setFrame_type(ACK_SIGNAL);
+    packet->setACK_nr(expectedSeqNum);
+    send(packet, NODE_OUTPUT);
+
+    // Write the sent ACK to the output file
+    outputFile << "Sent: ACK " << expectedSeqNum << endl;
+}
+
 void Node::handleMessage(cMessage *msg)
 {
     // Cast the message to Packet_Base
     Packet_Base *packet = check_and_cast<Packet_Base *>(msg);
 
     if (msg->isSelfMessage())
-    {
-        int msgIndex = std::atoi(msg->getName());
-        EV << "Checking timeout\n";
-        if (msgIndex < this->start)
-            return;
-        else
-        {
-            EV << "Timed out\n";
-            this->start = msgIndex;
-            this->index = msgIndex;
-        }
-    }
+        this->checkTimeout(std::atoi(msg->getName()));
+
     // Create output.txt file to log the messages events into it
-    std::ofstream outputFile("../output/output.txt", std::ios_base::app);
+    outputFile.open("../output/output.txt", std::ios_base::app);
 
     // If the message is the start signal
     if (strcmp(packet->getPayload(), START_SIGNAL) == 0)
     {
-        std::string inputMessage;
-
-        std::ifstream inputFile;
-
-        // If we are in Node 0, then read the messages from input0.txt
-        if (strcmp(getName(), NODE0) == 0)
-        {
-            inputFile.open("../inputs/input0.txt");
-        }
-        // Else if we are in Node 1, then read messages from input1.txt
-        else if (strcmp(getName(), NODE1) == 0)
-        {
-            inputFile.open("../inputs/input1.txt");
-        }
-
-        // Read messages from the input file
-        while (std::getline(inputFile, inputMessage))
-        {
-            // The first part will be errors
-            std::string errors = strtok((char *)inputMessage.c_str(), " ");
-
-            // The rest of the data will be the message to be sent
-            std::string message = inputMessage.substr(errors.size() + 1);
-
-            // Store the errors-message pair in the data vector
-            data.emplace_back(std::make_pair(errors, message));
-        }
-
-        // Close the input file
-        inputFile.close();
-
-        /* Construct the packet */
-        // 1.Apply byte stuffing framing to the message payload
-        std::string frame = Node::framing(data[index++].second);
-        // 2.Set the trailer to the parity byte
-        packet->setTrailer(Node::getParity(frame));
-        // 3.Set the payload to the frame
-        packet->setPayload(frame.c_str());
-        // 4.Set the header to the sequence number
-        packet->setHeader(sequenceNumber++);
-
-        // Send the first packet to the other node
-        send(packet, NODE_OUTPUT);
-
-        int TO = getParentModule()->par("TO").intValue();
-        Packet_Base *timeout = new Packet_Base(std::to_string(index).c_str());
-        scheduleAt(simTime() + exponential(TO), timeout);
-
-        // Write the sent message to the output file
-        outputFile << "Sent: " << packet->getPayload() << endl;
+        readFileMsg();                                     // Read the messages from the input file
+        packet = createPacket(packet, data[index].second); // Construct the packet
+        sendPacket(packet);                                // Send the first packet to the other node
+        index++;                                           // Increment the index
     }
     // If the message is an ACK signal, then send the next message
     else if (packet->getFrame_type() == ACK_SIGNAL)
     {
-        // If we didn't reach the end of the vector yet
-        if (index < data.size())
+        // If we reached the end of the vector, then finish the simulation
+        if (index == data.size())
         {
-
-            /* Construct the packet */
-            // 1.Apply byte stuffing framing to the message payload
-            std::string frame = Node::framing(data[index++].second);
-            // 2.Set the trailer to the parity byte
-            packet->setTrailer(Node::getParity(frame));
-            // 3.Set the payload to the frame
-            packet->setPayload(frame.c_str());
-            // 4.Set the header to the sequence number
-            packet->setHeader(sequenceNumber++);
-
-            // Send the next packet to the other node
-            send(packet, NODE_OUTPUT);
-
-            int TO = getParentModule()->par("TO").intValue();
-            Packet_Base *timeout = new Packet_Base(std::to_string(index).c_str());
-            scheduleAt(simTime() + exponential(TO), timeout);
-
-            // Write the sent message to the output file
-            outputFile << "Sent: " << packet->getPayload() << endl;
+            cancelAndDelete(packet); // Delete the packet object
+            return finish();         // Finish the simulation
         }
-        // Else, finish the simulation
-        else
-        {
-            // Delete the message object
-            cancelAndDelete(packet);
 
-            // Finish the simulation
-            finish();
-        }
+        packet = createPacket(packet, data[index].second); // Construct the packet
+        sendPacket(packet);                                // Send the next packet to the other node
+        index++;                                           // Increment the index
     }
     // Logic for the receiving node
     else
     {
-        std::string frame = packet->getPayload();
-        char parity = packet->getTrailer();
-
-        // Check if the parity byte is error free
-        if (Node::checkParity(frame, parity))
-        {
-            outputFile << "Received: " << frame << endl;
-        }
-        else
-        {
-            outputFile << "Error detected!" << endl;
-        }
-
-        // Send an ACK signal to the sender node to send the next message
-        packet->setFrame_type(ACK_SIGNAL);
-        send(packet, NODE_OUTPUT);
+        int seqNum = receivePacket(packet);
+        sendAck(packet, seqNum);
     }
 
     // Close the output file
--- System/src/Node.h
+++ System/src/Node.h
@@ -7,30 +7,45 @@
 #include <vector>
 #include <utility>
 #include <bitset>
+#include "Packet_m.h"
 
 using namespace omnetpp;
 
 class Node : public cSimpleModule
 {
 private:
+    /* CONSTANTS */
+    const int WS = getParentModule()->par("WS").intValue();
+    const int TO = getParentModule()->par("TO").intValue();
+
+    // A file stream to write the output to
+    std::ofstream outputFile;
+
     // A vector of string pairs to store the errors binary string and the message
     std::vector<std::pair<std::string, std::string>> data;
 
     // A pointer to the index of the message that should be sent
     int index = 0;
 
-    // initialize the sequence number to 0
-    int sequenceNumber = 0;
-
     // Start of window
     int start = 0;
+
+    // Expected sequence number
+    int expectedSeqNum = 0;
 
 protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
-    std::string framing(std::string payload);
+    void readFileMsg();
     char getParity(std::string frame);
+    void checkTimeout(int msgIndex);
+    void sendPacket(Packet_Base *packet);
     bool checkParity(std::string frame, char expectedParity);
+    int receivePacket(Packet_Base *packet);
+    void sendAck(Packet_Base *packet, int seqNum);
+    bool receiveAck(Packet_Base *packet);
+    std::string framing(std::string payload);
+    Packet_Base *createPacket(Packet_Base *oldPacket, std::string newPayload);
 };
 
 #endif
